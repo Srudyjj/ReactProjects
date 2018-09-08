a947bd532a757bb012b2e752fd606483aab25a5c
Turbinist <srudyjju@gmail.com>
2018-09-08 21:24:05 +0300

    Smth

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Contact />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+        </div>
       </div>
     );
   }
