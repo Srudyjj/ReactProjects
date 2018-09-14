9e75ac6f6fa60bc0eae67b6c431c992f386ce3ff
Turbinist <srudyjju@gmail.com>
2018-09-14 22:43:34 +0300

    Add all

--- contactmanager/README.md
+++
@@ -1,3 +0,0 @@
-# react-contact-manager
-
-React/Context API
--- contactmanagerredux/src/components/layout/TextInputGroup.js
+++ contactmanagerredux/src/components/layout/TextInputGroup.js
@@ -14,26 +14,33 @@ const TextInputGroup = ({
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} placeholder={placeholder} className={classnames("form-control form-control-lg", {"is-invalid":error})} value={value}
-        onChange={onChange} />
+      <input
+        type={type}
+        name={name}
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
-  )
-}
+  );
+};
 
 TextInputGroup.propTypes = {
-  name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   error: PropTypes.string
-}
+};
 
 TextInputGroup.defaultProps = {
-  type: "text"
-}
+  type: 'text'
+};
 
-export default TextInputGroup
-
+export default TextInputGroup;
